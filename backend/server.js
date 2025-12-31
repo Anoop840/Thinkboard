37de3670472628fedb5a3e89f8f5fe9d1fbf4b61
Anoop Agrawal <anoopag136@gmail.com>
2025-12-31 18:49:46 +0530

    feat: update backend dependencies and configure CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import router from "./routes/notesRoutes.js";
+
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
+app.use(rateLimiter);
+
 app.use("/api/notes", notesRoutes);
 
 connectDB().then(() => {
